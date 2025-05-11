0303a9ed0fdda8a108feaaa6124bb026ca1cd6d4
artteri <mikhail.kazakov.qa@gmail.com>
2025-05-11 22:13:33 +0500

    Second commit

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -4,9 +4,12 @@ test("Calendar validations", async ({ page }) => {
   const monthNumber = "6";
   const date = "15";
   const year = "2027";
-  const expectedList = [monthNumber, date, year];
+  const expectedDate = `${year}-${monthNumber.padStart(2, "0")}-${date.padStart(
+    2,
+    "0"
+  )}`;
 
-  await test.step("Открываем страницу", async () => {
+  await test.step("Открываем сайт", async () => {
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
   });
 
@@ -14,31 +17,31 @@ test("Calendar validations", async ({ page }) => {
     await page.locator(".react-date-picker__inputGroup").click();
   });
 
-  await test.step("Переходим к выбору года", async () => {
+  await test.step("Переход к выбору года", async () => {
     await page.locator(".react-calendar__navigation__label").click();
     await page.locator(".react-calendar__navigation__label").click();
   });
 
-  await test.step(`Выбираем год: ${year}`, async () => {
+  await test.step(`Выбор года: ${year}`, async () => {
     await page.getByText(year).click();
   });
 
-  await test.step(`Выбираем месяц: ${monthNumber}`, async () => {
+  await test.step(`Выбор месяца: ${monthNumber}`, async () => {
     await page
       .locator(".react-calendar__year-view__months__month")
       .nth(Number(monthNumber) - 1)
       .click();
   });
 
-  await test.step(`Выбираем день: ${date}`, async () => {
+  await test.step(`Выбор дня: ${date}`, async () => {
     await page.locator(`//abbr[text()='${date}']`).click();
   });
 
-  await test.step("Проверяем введённую дату", async () => {
-    const inputs = await page.locator(".react-date-picker__inputGroup input");
-    for (let index = 0; index < (await inputs.count()); index++) {
-      const value = await inputs.nth(index).getAttribute("value");
-      expect(value).toEqual(expectedList[index]);
-    }
+  await test.step("Проверка выбранной даты", async () => {
+    const fullDateInput = page
+      .locator(".react-date-picker__inputGroup input")
+      .first();
+    const actualValue = await fullDateInput.getAttribute("value");
+    expect(actualValue).toEqual(expectedDate);
   });
 });
